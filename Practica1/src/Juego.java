d3f68f67f8b66537d80fe273cf28317675111d62
huwin <huwin_1290@hotmail.es>
2014-09-04 00:05:20 -0600

    menu, quitado de cartas y dar inico de turno

--- Practica1/src/Juego.java
+++ Practica1/src/Juego.java
@@ -4,10 +4,11 @@ import java.util.Scanner;
 
 public class Juego {
 	
-	public static int x,n,c1,c2,c3,c4,c5,c6,c7,c8,c9;
-	public static String carta1="Guard", carta2="Guard",carta3="Guard",carta4="Guard",carta5="Guard";
-	public static String carta6="Priest", carta7="Priest",carta8="Baron",carta9="Baron",carta10="Handmaid";
-	public static String carta11="Handmaid", carta12="PrinCe",carta13="Prince",carta14="King",carta15="Countess",carta16="Princess";
+	public static int x,n,c1,c2,c3,c4,k1,k2,k3,k4,k5,k6,k7,k8,k9,k10,k11,k12,k13,k14,k15,k16;
+	public static String carta1="(1) Guard", carta2="(1) Guard",carta3="(1) Guard",carta4="(1) Guard",carta5="(1) Guard";
+	public static String carta6="(2) Priest", carta7="(2) Priest",carta8="(3) Baron",carta9="(3) Baron",carta10="(4) Handmaid";
+	public static String carta11="(4) Handmaid", carta12="(5) PrinCe",carta13="(5) Prince",carta14="(6) King",carta15="(7) Countess",carta16="(8) Princess";
+	public static String j,p;
 
 
 
@@ -22,7 +23,7 @@ public class Juego {
 			//DECLARACION DE VARIABLES LOCALES
 			
 			int num=0, regresar=0,tok=0;
-			String j,p;
+			
 				
 				
 			Scanner entrada=new Scanner(System.in);
@@ -104,125 +105,113 @@ public class Juego {
 					System.out.println("LAS CARTAS REMOVIDAS SON:\n ");
 			if (c1==1 || c1==2 || c1==3 || c1==4 || c1==5){
 
-				System.out.println("(1)  "+carta1);
+				System.out.println(""+carta1);
 			}
 			else if(c1==6 || c1==7){
-				System.out.println("(2) "+carta6);
+				System.out.println(""+carta6);
 			}
 			else if(c1==8 ||c1==9){
-				System.out.println("(3) "+carta8);
+				System.out.println(""+carta8);
 			}
 			else if(c1==10 || c1==11){
-				System.out.println("(4) "+carta10);
+				System.out.println(""+carta10);
 			}
 			else if(c1==12 || c1==13){
-				System.out.println("(5) "+carta12);
+				System.out.println(" "+carta12);
 			}
 			else if(c1==14){
-				System.out.println("(6) "+carta14);
+				System.out.println(" "+carta14);
 			}
 			else if(c1==15){
-				System.out.println("(7) "+carta15);
+				System.out.println(" "+carta15);
 			}
 			else if(c1==16){
-				System.out.println("(8) "+carta16);
+				System.out.println(""+carta16);
 			}
 			
 			
 			if (c2==1 || c2==2 || c2==3 || c2==4 || c2==5){
 
-				System.out.println("(1)  "+carta1);
+				System.out.println(" "+carta1);
 			}
 			else if(c2==6 || c2==7){
-				System.out.println("(2) "+carta6);
+				System.out.println(""+carta6);
 			}
 			else if(c2==8 ||c2==9){
-				System.out.println("(3) "+carta8);
+				System.out.println(""+carta8);
 			}
 			else if(c2==10 || c2==11){
-				System.out.println("(4) "+carta10);
+				System.out.println(""+carta10);
 			}
 			else if(c2==12 || c2==13){
-				System.out.println("(5) "+carta12);
+				System.out.println(""+carta12);
 			}
 			else if(c2==14){
-				System.out.println("(6) "+carta14);
+				System.out.println(""+carta14);
 			}
 			else if(c2==15){
-				System.out.println("(7) "+carta15);
+				System.out.println(""+carta15);
 			}
 			else if(c2==16){
-				System.out.println("(8) "+carta16);
+				System.out.println(""+carta16);
 			}
 			
 			
 			
 			if (c3==1 || c3==2 || c3==3 || c3==4 || c3==5){
 
-				System.out.println("(1)  "+carta1);
+				System.out.println(""+carta1);
 			}
 			else if(c3==6 || c3==7){
-				System.out.println("(2) "+carta6);
+				System.out.println(""+carta6);
 			}
 			else if(c3==8 ||c3==9){
-				System.out.println("(3) "+carta8);
+				System.out.println(""+carta8);
 			}
 			else if(c3==10 || c3==11){
-				System.out.println("(4) "+carta10);
+				System.out.println(""+carta10);
 			}
 			else if(c3==12 || c3==13){
-				System.out.println("(5) "+carta12);
+				System.out.println(""+carta12);
 			}
 			else if(c3==14){
-				System.out.println("(6) "+carta14);
+				System.out.println(""+carta14);
 			}
 			else if(c3==15){
-				System.out.println("(7) "+carta15);
+				System.out.println(""+carta15);
 			}
 			else if(c3==16){
-				System.out.println("(8) "+carta16);
+				System.out.println(""+carta16);
 			}
 					
 			
 			if (c4==1 || c4==2 || c4==3 || c4==4 || c4==5){
 
-				System.out.println("(1)  "+carta1);
+				System.out.println(""+carta1);
 			}
 			else if(c4==6 || c4==7){
-				System.out.println("(2) "+carta6);
+				System.out.println(""+carta6);
 			}
 			else if(c4==8 ||c4==9){
-				System.out.println("(3) "+carta8);
+				System.out.println(""+carta8);
 			}
 			else if(c4==10 || c4==11){
-				System.out.println("(4) "+carta10);
+				System.out.println(""+carta10);
 			}
 			else if(c4==12 || c4==13){
-				System.out.println("(5) "+carta12);
+				System.out.println(""+carta12);
 			}
 			else if(c4==14){
-				System.out.println("(6) "+carta14);
+				System.out.println(""+carta14);
 			}
 			else if(c4==15){
-				System.out.println("(7) "+carta15);
+				System.out.println(""+carta15);
 			}
 			else if(c4==16){
-				System.out.println("(8) "+carta16);
+				System.out.println(""+carta16);
 			}
 					
-					// aleatorio para ver quien empieza el juego
 					
-					Random turno=new Random();
-					int n=1+turno.nextInt(2);
-					if (n==1){
-						System.out.println("Tu empiezas el juego "+j);
-						System.out.println("El rival pc es el Segundo ");
-					}
-						else
-						{
-							System.out.println(""+j+"eres el segundo en jugar");
-							System.out.println("Tu rival pc es quien empieza el juego ");
-						}
 				
 				}
 				else if( acum==0){
@@ -272,11 +261,28 @@ public class Juego {
 				break;
 			}
 				
+		
+			
+				
 				
 			
 		
-		
-		}
+			// aleatorio para ver quien empieza el juego
+			
+			Random turno=new Random();
+			int n=turno.nextInt(2);
+			if (n==0){
+				System.out.println("Tu empiezas el juego "+j);
+				System.out.println("El rival pc es el Segundo ");
+			}
+				else
+				{
+					System.out.println(""+j+"eres el segundo en jugar");
+					System.out.println("Tu rival pc es quien empieza el juego ");
+				}
+				
+			
+		}//TERMINA MAIN
 
 	}
 
